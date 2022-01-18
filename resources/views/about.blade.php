24ceda048fe215b5bcfc0595211a2214e2807845
unknown <putraraditya972@gmail.com>
2022-01-18 14:32:15 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Raditya | About</title>
+</head>
+<body>
+    <h1> Halaman About <h1>
+    <h3> Raditya Putra A.P </h3>
+    <p> putraraditya972@gmail.com </p>
+    <img src="images/radit.jpg" alt="radit" width="200px">
+</body>
+</html>
