205299ccbf7cea45f27c3c66df935a1f0ad51b88
unknown <putraraditya972@gmail.com>
2022-02-10 13:27:35 +0700

    upload modul 4

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,25 +1,24 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-    <div class="container">
-    <a class="navbar-brand" href="/">Radit</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+  <div class="container">
+    <a class="navbar-brand" href="#">Raditya</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link {{ ($title === 'Home') ? 'active' : ''}}" aria-current="page" href="/">Home</a>
+          <a class="nav-link {{ ( $title === 'Home') ? 'active' : '' }}" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ ($title === 'About') ? 'active' : ''}}" aria-current="page" href="/about">About</a>
+          <a class="nav-link {{ ( $title === 'About') ? 'active' : '' }}">About</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ ($title === 'Gallery') ? 'active' : ''}}" aria-current="page" href="/gallery">Gallery</a>
+          <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}">Gallery</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="/contacts">Contact us</a>
+          <a class="nav-link {{ ( $title === 'Contacts' ) ? 'active' : '' }}" href="/contacts">Contact Us</a>
         </li>
-        
-            </ul>
-            </div>
-        </div>
-    </nav>
+      </ul>
+    </div>
+  </div>
+</nav>
